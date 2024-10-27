9d7fc12cbf1c43906ce3dc49970333be9a8be743
moose23967 <moose23967@gmail.com>
2024-10-28 00:42:02 +0300

    Implemented

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import Elysia from 'elysia';
+
+type Social =
+	| 'github'
+	| 'instagram'
+	| 'soundcloud'
+	| 'spotify'
+	| 'telegram'
+	| 'vk';
+
+const socials: Record<Social, string> = {
+	github: 'https://github.com/moose23967',
+	instagram: 'https://instagram.com/musu23967',
+	soundcloud: 'https://on.soundcloud.com/MXHoANuGsfFwbQys7',
+	spotify: 'https://open.spotify.com/user/31jgugynqmzpiwoxdkjqor6degdu',
+	telegram: 'https://t.me/moose23967',
+	vk: 'https://vk.com/musu23967',
+};
+
+new Elysia({
+	name: 'moose23967-ru',
+})
+	.get('/', ({ redirect }) => redirect('https://campsite.bio/moose23967'))
+	.get('/socials/:social', ({ redirect, params: { social } }) => {
+		if (social in socials) {
+			return redirect(socials[social as Social]);
+		}
+
+		return 'Meow';
+	})
+	.listen(10_000);
